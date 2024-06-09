896198ef92562f0fc870e7b560e3797413416af3
vados559 <ilyxaaa03@gmail.com>
2024-06-10 02:37:48 +0300

    Покарасил кнопки, я серьезно

--- app/forms.py
+++ app/forms.py
@@ -8,21 +8,21 @@ class RegisterForm(FlaskForm):
     Password = PasswordField('Пароль', validators=[DataRequired()])
     CheckPassword = PasswordField('Подтвердите пароль', validators=[DataRequired()])
     Role = SelectField('Ваша роль', choices=[('Студент'), ('Преподаватель')], validators=[DataRequired()])
-    Submit = SubmitField('Регситрация', validators=[DataRequired()])
+    Submit = SubmitField('Регистрация', validators=[DataRequired()], render_kw={"class": "w-100 mb-2 btn btn-lg rounded-3 btn-primary"})
 
 class AuthForm(FlaskForm):
     Email = StringField('Электронная почта', validators=[DataRequired(), Length(min=1, max=128), Email()])
     Password = PasswordField('Пароль', validators=[DataRequired()])
-    Submit = SubmitField('Войти')
+    Submit = SubmitField('Войти', render_kw={"class": "w-100 mb-2 btn btn-lg rounded-3 btn-primary"})
 
 class CourseForm(FlaskForm):
     NameCourse = StringField('Название курса', validators=[DataRequired()])
     Photo = FileField('Фото курса', validators=[DataRequired(),FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
     DescriptionCourse = StringField('Описание курса', validators=[DataRequired()])
-    Submit = SubmitField('Добавить')
+    Submit = SubmitField('Добавить', render_kw={"class": "w-100 mb-2 btn btn-lg rounded-3 btn-primary"})
 
 class LessonForm(FlaskForm):
     NameLesson = StringField('Название урока', validators=[DataRequired()])
     FileLesson = FileField('Материал урока', validators=[DataRequired(), FileAllowed(['mp4', 'avi', 'mov', 'mkv'], 'Video only!')])
     TextLesson = TextAreaField('Текст урока', validators=[DataRequired()], render_kw={"class": "form-control", "style": "height: 200px;"})
-    Submit = SubmitField('Добавить')
+    Submit = SubmitField('Добавить', render_kw={"class": "w-100 mb-2 btn btn-lg rounded-3 btn-primary"})
